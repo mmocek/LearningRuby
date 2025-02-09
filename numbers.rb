bb93b47e69c7bb7169e4a0104c70d2c064f87270
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-09 15:35:12 +0100

    podstawowe działania na liczbach w Ruby

---
+++ numbers.rb
@@ -0,0 +1,8 @@
+puts -21.37 #int, float + ujemne
+puts 5 % 3 #podstawowe działania arytmetyczne, modulo
+
+num = 2.21
+puts ("moja ulubiona liczba " + num.to_s) #zamiana int na string zeby dzialalo
+puts num.ceil #po kropce sa rozne metody
+
+puts Math.sqrt(36) #pierwiastki, logarytmy itd, klasa Math
