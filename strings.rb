7a909e6b8d6fde4dade8546eebd3ceffa7d868af
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-09 15:23:35 +0100

    podstawowe działania na stringach w Ruby

---
+++ strings.rb
@@ -0,0 +1,8 @@
+puts "\"Hello\" World!" #tak sie printuje cudzysłów
+puts "elo\nżelo" #\n tworzy nowa linie miedzy stringami
+
+slowo = "Kocham ZUT!"
+puts slowo.include? "XD" #po kromce mozna uzywać różnych metod modyfikowania teksty
+puts slowo[0, 3] #indeksy, 0 pierwsza pozycja stringu, [0, 3] 3 oznacza ile znakow
+
+puts "essa".upcase #nie trzeba zmiennych
