3caec71be20d3b394d789112631583b0b0b54056
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-10 18:22:32 +0100

    madlibs gra fajna

---
+++ MadLibsGame.rb
@@ -0,0 +1,17 @@
+#gierka fajna co nie
+#polega na tym ze losowe slowa wpisujesz se do jakiegos zdanka
+#i wychodzi smieszna historyjka
+
+puts("Podaj losowa osobe: ")
+czlowiek = gets.chomp
+puts("Podaj wiek: ")
+wiek = gets.chomp
+puts("Podaj kierunek studiów: ")
+kierunek_studiów = gets.chomp
+
+
+puts (czlowiek + " to mega kox")
+puts ("Lat ma " + wiek)
+puts ("I studiuje " + kierunek_studiów)
+
+#mega super gierka polecam
