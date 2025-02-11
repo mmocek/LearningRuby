6c8c00e8538f1a67be75d8b419e65ea365f667d3
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-11 12:00:37 +0100

    tak dziala return pogchamp

---
+++ returnStatement.rb
@@ -0,0 +1,7 @@
+def cube(num)
+  return num * num * num, 70-1
+  jakim cudem ten program działa? jak to komentarz nie jest xpp #to jest komentarz btw
+end
+
+puts cube(10)[0] #mozna wybrac ktora czesc returna brac
+
