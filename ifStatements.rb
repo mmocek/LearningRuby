bf6f3be68364b4c1e0cbaa83aa3d94d9878a5294
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-12 14:10:07 +0100

    ify

---
+++ ifStatements.rb
@@ -0,0 +1,10 @@
+ismale = true
+istall = false
+
+if ismale and istall #and - oba true, or - jedno true
+  puts "You are tall male"
+elsif ismale and !istall
+  puts "You are short male"
+else
+  puts "neither"
+end
