5eff7c272e431c41daa4cf6fc997b70e71608200
Mikołaj Mocek <mikolaj.mocek@gmail.com>
2025-02-15 12:04:33 +0100

    fajna gierka cnie

---
+++ forLoops.rb
@@ -0,0 +1,17 @@
+friends = ["Martin", "Ewron", "Adam", "Cypu", "Jurek"]
+#cos w stylu python
+'''for friend in friends
+  puts friend
+end'''
+#to samo ale inaczej
+'''friends.each do |friend|
+  puts friend
+end'''
+#czadowy sposob
+for index in 0..5
+  puts friends[index]
+end
+#mega mocne
+6.times do |xd|
+  puts xd
+end
